891a6f51b56d86ea3596ce5c130f933acabc25b8
Mostafa Rezapour <48005311+MostafaRezapour@users.noreply.github.com>
2020-10-22 20:05:39 -0400

    Add files via upload

---
+++ detectinv.m
@@ -0,0 +1,70 @@
+% Author: Mostafa Rezapour
+% The input of this function is an m by n matrix, and the output are: 1. A
+% matrix whose columns are the pivot columns of the input matrix A 2. A
+% vector whose elements are the pivot columns indices. 
+% W e will use this function for our future projects. For instance, if you
+% are interested to find a basis of the column space of a matrix, this code
+% will give a basis in the matrix format. 
+% 
+
+
+
+% My dear students in MST 121-D at Wake Fores- Please watch the tutorial
+% video that I have recoreded and posted on Canvas for you. In the video I
+% start from scratch and go through this code to show you what the reason
+% behind each code line is.
+
+function detectinv(A)
+Pivot_columns_matrix=[];
+ind=[];
+[m n]=size(A);
+% If A is a square matrix (m=n) and its rank is equal to the noumber of
+% rows or columns then the columns of A are linearly indepent
+if m==n
+    if rank(A)==m
+        f=msgbox('The columns of A are linearly independent', 'Help','help')
+    elseif rank(A)~=m 
+          R=rref(A);
+     [m n]=size(A);
+     for i=1:m
+         if sum(abs(R(i,:)))~=0
+             r=R(i,:);
+             v=find(r);
+             Pivot_columns_matrix=[Pivot_columns_matrix A(:,v(1))];
+             ind=[ind v(1)];
+         end
+     end
+     p=Pivot_columns_matrix;
+     disp('The columns of the following matrix are the pivot columns of the origonal matrix:')
+     disp(Pivot_columns_matrix)
+      disp(' The pivot columns indices are:')
+     disp(ind)
+        return
+    end
+end
+
+ if m~=n
+     R=rref(A);
+     [m n]=size(A);
+     for i=1:m
+         if sum(abs(R(i,:)))~=0
+             r=R(i,:);
+             v=find(r);
+             Pivot_columns_matrix=[Pivot_columns_matrix A(:,v(1))];
+             ind=[ind v(1)];
+         end
+     end
+     p=Pivot_columns_matrix;
+     disp('The columns of the following matrix are the pivot columns of the origonal matrix:')
+     disp(Pivot_columns_matrix)
+     disp(' The pivot columns indices are:')
+     disp(ind)
+    
+
+ end
+             
+
+
+
+%
+end
